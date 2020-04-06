168edd1b325afc93183589507c8bfbdd56eb4f52
Boyan Xu <dominictxu1@gmail.com>
2020-04-06 14:29:15 -0700

    refactored code

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.resolve(__dirname + "public\index.html"));
+app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname + "/public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT:${PORT}`));
