6bca07d281e5dc52bf45247d398b9f219a11525f
Boyan Xu <dominictxu1@gmail.com>
2020-04-06 09:42:09 -0700

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.resolve(__dirname + "public\index.html"));
 });
 
